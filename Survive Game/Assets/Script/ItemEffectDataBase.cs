cea4c0120b37fa081ccfdb3e5557e42a73ffdd34
Dev-SSW <sgg919@naver.com>
2023-02-12 19:53:31 +0900

    small update

---
+++ Survive Game/Assets/Script/ItemEffectDataBase.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemEffect
+{
+    public string itemName; //아이템 이름. (키값)
+    public string part; //부위
+    public int num; //수치
+}
+
+public class ItemEffectDataBase : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
